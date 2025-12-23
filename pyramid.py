1b0ba87cc3b82ebc6bb0ad66c10f15db74ddf1b9
positive132 <nekit_ich@tutamail.com>
2025-12-23 09:40:19 +0300

    pyramid.py

--- pyramid.py
+++ pyramid.py
@@ -1,4 +1,3 @@
-import math 
 func = lambda s1, s2, h : 1/3 * h * (s1 + s2 + (s1 * s2) ** 1/2)
 def main():
     s1 = float(input('Введите площадь нижнего основания в м^3: '))
@@ -8,3 +7,4 @@ def main():
     print('ОбЪём усечённой пирамиды составляет:',V)
 if __name__ == "__main__":
     main()    
+
